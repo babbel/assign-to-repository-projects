5aa252d3bb30d6382b7f89e19a8450607f6fbeeb
Jahn Saito <jahn.saito@gmail.com>
2023-05-12 10:55:35 +0200

    Add one integration test

---
+++ __tests__/projects.js
@@ -0,0 +1,142 @@
+import nock from 'nock'; // https://github.com/nock/nock
+
+import { Octokit } from '@octokit/core';
+import { paginateGraphql } from '@octokit/plugin-paginate-graphql';
+import { RepositoryProjectsManager } from '../projects.js'; // eslint-disable-line import/extensions
+
+const GraphQlOctokit = Octokit.plugin(paginateGraphql);
+const octokit = new GraphQlOctokit({ auth: 'fake-token-value' }); // don't use default GITHUB_TOKEN token from env
+
+const rpm = new RepositoryProjectsManager({
+  owner: 'babbel-sandbox',
+  repository: 'test-repo-jsaito-3',
+  octokit,
+});
+
+describe('RepositoryProjectsManager.assing() posts requests to the API', () => {
+  beforeEach(() => {
+    nock.restore();
+    nock.activate();
+  });
+
+  afterEach(() => {
+    nock.restore();
+  });
+
+  test('when the PR is already assigned to a project', async () => {
+    const titles = [
+      'layer-100/bar',
+    ];
+
+    const pullRequestNumber = 'PR_kwDOJSgWus5QXGKx';
+
+    nock('https://api.github.com')
+      .post('/graphql', (body) => /.*organization.login:.*/.test(body.query))
+      .reply(200, {
+        data: {
+          organization: {
+            id: 'O_kgDOAnsQgg',
+            name: 'Babbel Sandbox',
+          },
+        },
+      })
+      .post('/graphql', (body) => /projectsV2.first:/.test(body.query))
+      .reply(
+        200,
+        {
+          data: {
+            repository: {
+              name: 'example-repository',
+              id: 'R_0000000001',
+              projectsV2: {
+                nodes: [
+                  {
+                    id: 'PVT_kwDOAnsQgs4AQFxl',
+                    title: 'layer-100/bar',
+                    number: 1099,
+                    fields: {
+                      nodes: [{}, {},
+                        {
+                          id: 'PVTSSF_lADOAnsQgs4AQFxlzgKRc_A',
+                          name: 'Status',
+                          options: [
+                            {
+                              id: 'f75ad846',
+                              name: 'Todo',
+                            },
+                            {
+                              id: '47fc9ee4',
+                              name: 'In Progress',
+                            },
+                            {
+                              id: '98236657',
+                              name: 'Done',
+                            },
+                          ],
+                        }, {}, {}],
+                    },
+                  },
+                ],
+                pageInfo: {
+                  hasNextPage: false,
+                  endCursor: 'Nw',
+                },
+              },
+            },
+          },
+        },
+      )
+      .post('/graphql', (body) => /addProjectV2ItemById/.test(body.query))
+      .reply(
+        200,
+        {
+          data: {
+            addProjectV2ItemById: {
+              item: {
+                id: 'PVTI_lADOAnsQgs4AQFxlzgGroec',
+              },
+            },
+          },
+        },
+      )
+      .post('/graphql', (body) => /updateProjectV2ItemFieldValue/.test(body.query))
+      .reply(
+        200,
+        {
+          data: {
+            updateProjectV2ItemFieldValue: { projectV2Item: { id: 'PVTI_lADOAnsQgs4AQFxlzgGroec' } },
+          },
+        },
+      )
+      .post('/graphql', (body) => /on PullRequest/.test(body.query))
+      .twice()
+      .reply(
+        200,
+        {
+          data: {
+            node:
+             {
+               number: 74,
+               projectsV2: {
+                 nodes: [
+                   {
+                     id: 'PVT_kwDOAnsQgs4AQFxl',
+                     title: 'layer-100/bar',
+                   },
+                 ],
+                 pageInfo: {
+                   hasNextPage: false,
+                   endCursor: 'MQ',
+                 },
+               },
+             },
+          },
+        },
+      );
+
+    const outputProjects = await rpm.assign(pullRequestNumber, titles);
+    const outputTitles = outputProjects.map((p) => p.title);
+
+    expect(outputTitles).toEqual(titles);
+  });
+});
