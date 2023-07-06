fd5610ead1f55b553912aae369d3757362d84768
Jahn Saito <jahn.saito@gmail.com>
2023-07-06 16:30:54 +0200

    Mv API calls into ApiWrapper

--- projects.js
+++ projects.js
@@ -1,25 +1,25 @@
 class RepositoryProjectsManager {
-  constructor({ owner, repository, octokit }) {
+  constructor({ owner, repository, apiWrapper }) {
+    this.apiWrapper = apiWrapper;
     this.owner = owner;
     this.repositoryName = repository;
-    this.octokit = octokit;
     this.clientMutationId = `assign-to-repository-projects-${owner}-${repository}`;
   }
 
   async assign(pullRequestId, titles) {
     await this.#init();
 
-    const assignedProjects = await this.#fetchAssignedProjects({ pullRequestId });
+    const assignedProjects = await this.apiWrapper.fetchAssignedProjects({ pullRequestId });
     await this.#assignPRToProjects(pullRequestId, titles, assignedProjects);
     await this.#unassignPRFromProjects(pullRequestId, titles, assignedProjects);
 
-    return this.#fetchAssignedProjects({ pullRequestId });
+    return this.apiWrapper.fetchAssignedProjects({ pullRequestId });
   }
 
   async #init() {
-    this.organization = this.#fetchOrganization({ owner: this.owner });
+    this.organization = this.apiWrapper.fetchOrganization({ owner: this.owner });
 
-    const repository = await this.#fetchRepositoryAndProjects({
+    const repository = await this.apiWrapper.fetchRepositoryAndProjects({
       owner: this.owner,
       repositoryName: this.repositoryName,
     });
@@ -37,10 +37,10 @@ class RepositoryProjectsManager {
 
     for await (const project of projects) {
       // async, because more than 5 breaks API endpoint
-      const item = await this.#assignPRtoProject({
+      const item = await this.apiWrapper.assignPRtoProject({
         project,
         pullRequestId,
-        clientMutationId: this.clientMutationId
+        clientMutationId: this.clientMutationId,
       });
 
       // at creation, items can only be assigned to projecs but initially
@@ -58,7 +58,7 @@ class RepositoryProjectsManager {
     const statusField = project.fields.nodes.find((n) => Object.keys(n) !== 0 && n.name === 'Status');
     const todoOption = statusField.options.find((o) => o.name === 'Todo');
 
-    return this.#updateItemFieldValue({
+    return this.apiWrapper.updateItemFieldValue({
       project,
       item,
       statusField,
@@ -73,184 +73,9 @@ class RepositoryProjectsManager {
 
     for await (const project of projects) {
       // async, because more than 5 breaks API endpoint
-      const item = await this.#fetchItemForPRId({ project, pullRequestId });
-      await this.#deleteProjectItem({ project, item, clientMutationId: this.clientMutationId } );
+      const item = await this.apiWrapper.fetchItemForPRId({ project, pullRequestId });
+      await this.apiWrapper.deleteProjectItem({ project, item, clientMutationId: this.clientMutationId });
     }
-  }
-
-  async #fetchAssignedProjects({ pullRequestId }) {
-    const { node: { projectsV2: { nodes } } } = await this.octokit.graphql.paginate(`
-      query paginate($cursor: String) {
-        node(id:"${pullRequestId}") {
-          ... on PullRequest {
-            number
-            projectsV2(first: 100, after: $cursor) {
-              nodes {
-                id
-                title
-              }
-              pageInfo {
-                hasNextPage
-                endCursor
-              }
-            }
-          }
-        }
-      }`);
-
-    return nodes;
-  }
-
-  // we need to know the item id of a PR in the project.
-  // we know this item exists
-  async #fetchItemForPRId({ project, pullRequestId }) {
-    const { node: { items: { nodes } } } = await this.octokit.graphql.paginate(`
-      query paginate($cursor: String) {
-        node(id:"${project.id}") {
-          ... on ProjectV2 {
-            number
-            items(first: 100, after: $cursor) {
-              nodes {
-                id
-                content {
-                  ... on PullRequest {
-                    id
-                  }
-                }
-              }
-              pageInfo {
-                hasNextPage
-                endCursor
-              }
-            }
-          }
-        }
-      }`);
-
-    return nodes.find((item) => item.content.id === pullRequestId);
-  }
-
-  // requires GitHub App installation token with read and write
-  // permissions for projects v2 and pull requests
-  async #deleteProjectItem({ project, item, clientMutationId }) {
-    const { deleteProjectV2Item: deletedItemId } = await this.octokit.graphql(`
-      mutation {
-        deleteProjectV2Item(
-          input: {
-            clientMutationId: "${clientMutationId}",
-            itemId: "${item.id}",
-            projectId: "${project.id}",
-          }
-        )
-        {
-          deletedItemId
-        }
-      }`);
-
-    return deletedItemId;
-  }
-
-  // the GitHub Action's event may only contain the "old" GraphQL node id.
-  // this produces deprecation warnings. as a workaround, look up the "new" ID.
-  // https://github.blog/changelog/label/deprecation/
-  async #fetchOrganization({ owner }) {
-    const { organization } = await this.octokit.graphql(
-      `query {
-         organization(login: "${owner}") {
-           id
-           name
-         }
-      }`,
-      {
-        headers: {
-          'X-Github-Next-Global-ID': '1',
-        },
-      },
-    );
-
-    return organization;
-  }
-
-  async #fetchRepositoryAndProjects({ owner, repositoryName }) {
-    const { repository } = await this.octokit.graphql.paginate(`
-      query paginate($cursor: String) {
-        repository(owner: "${owner}", name: "${repositoryName}") {
-          name
-          id
-          projectsV2(first: 100, after: $cursor) {
-            nodes {
-              id
-              title
-              number
-              fields(first: 5) {
-                nodes {
-                  ... on ProjectV2SingleSelectField {
-                    id
-                    name
-                    options {
-                      id
-                      name
-                    }
-                  }
-                }
-              }
-            }
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-          }
-        }
-      }`);
-    return repository;
-  }
-
-  // requires GitHub App installation token with read and write
-  // permissions for projects v2 and pull requests
-  async #assignPRtoProject({ pullRequestId, project, clientMutationId }) {
-    const { addProjectV2ItemById: { item } } = await this.octokit.graphql(`
-      mutation {
-        addProjectV2ItemById(
-          input: {
-            clientMutationId: "${clientMutationId}",
-            contentId: "${pullRequestId}",
-            projectId: "${project.id}",
-          }
-        )
-        {
-          item {
-            id
-          }
-        }
-      }`);
-
-    return item;
-  }
-
-  async #updateItemFieldValue({
-    clientMutationId, project, item, statusField, todoOption,
-  }) {
-    // https://docs.github.com/en/graphql/reference/mutations#updateprojectv2itemfieldvalue
-    const result = await this.octokit.graphql(`
-      mutation {
-        updateProjectV2ItemFieldValue(
-          input: {
-            clientMutationId: "${clientMutationId}",
-            projectId: "${project.id}",
-            fieldId: "${statusField.id}",
-            itemId: "${item.id}",
-            value: {
-              singleSelectOptionId: "${todoOption.id}"
-            }
-          }
-        ){
-           projectV2Item {
-             id
-           }
-         }
-      }`);
-
-    return result;
   }
 }
 
